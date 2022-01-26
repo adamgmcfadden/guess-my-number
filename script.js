b58d8e2a942794c6ec126884b7fd262b3d55e778
Adam Girard-McFadden <adam_m_20@hotmail.com>
2022-01-26 14:14:56 -0500

    guess checked for all cases

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Define the secret number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+// temp to check correct
+document.querySelector('.number').textContent = secretNumber;
+
 // Event listener for the "Check!" button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -11,5 +14,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No number entered!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too Low!';
   }
 });
