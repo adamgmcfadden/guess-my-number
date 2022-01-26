b59c864a9eb3a121fd9fd44025df8957774d1fe6
Adam Girard-McFadden <adam_m_20@hotmail.com>
2022-01-26 14:08:21 -0500

    random number between 1 and 20 being generated, and compared to user's guess for equality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Define the secret number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+// Event listener for the "Check!" button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number entered!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  }
+});
