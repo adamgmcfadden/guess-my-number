46431c4b19a543fae81d5c708a2a0632651647c3
Adam Girard-McFadden <adam_m_20@hotmail.com>
2022-01-26 14:42:37 -0500

    dynamic style changes to the DOM when game is won or lose

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ document.querySelector('.number').textContent = secretNumber;
 // Event listener for the "Check!" button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
+  //  when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number entered!';
+    // When player wins the game!
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
     // TODO refactor code later
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -24,7 +30,9 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ec1818';
     }
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
@@ -33,6 +41,7 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ec1818';
     }
   }
 });
